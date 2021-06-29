2d2f0f1768d4e3d06b2b96b99ae49897f590fcaf
liyanaMTM <85813499+liyanaMTM@users.noreply.github.com>
2021-06-29 08:31:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const fs = require('fs');
-const https = require('https');
-const port = 3000;
 
-app.set('/', 'html');
-//Static files
+const http = require('http');
+const port = (3000, () => console.info('listening on port'));
+
+//telling the path
+const path = require('path');
 app.use(express.static(path.join(__dirname, "/")));
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
@@ -15,33 +14,7 @@ app.use('/img', express.static(__dirname + 'public/img'))
 app.use('/object', express.static(__dirname + 'public/object'))
 app.use('/pattern', express.static(__dirname + 'public/pattern'))
 
-app.use(express.json());
-
-//Navigation
+//shows homepage
 app.get('',(req,res) => {
     res.render('index', {title: 'Home Page'})
 })
-
-app.get('/museum',(req,res) => {
-    res.render('museum', {title: 'List of Museums'})
-})
-
-app.get('/MTM',(req,res) => {
-    res.render('MTM', {title: 'MTM'})
-})
-
-app.get('/AR',(req,res) => {
-    res.render('AR', {title: 'AR'})
-})
-
-
-app.listen(port, () => console.info('listening on port'));
-
-// https.createServer({
-//     key: fs.readFileSync('server.key'),
-//     cert: fs.readFileSync('server.cert')
-// }, app)
-// .listen(5000, function () {
-//     console.log('listenn, ')
-// });
-

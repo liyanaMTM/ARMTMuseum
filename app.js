4faad885cc45c730945e557a7d600df823b6c337
liyanaMTM <85813499+liyanaMTM@users.noreply.github.com>
2021-06-29 08:26:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -45,8 +45,3 @@ app.listen(port, () => console.info('listening on port'));
 //     console.log('listenn, ')
 // });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
